418727a02794fba6f82799e436d9d2b55b3ff78c
Jojo <jojothomas1515@gmail.com>
2023-08-23 14:42:06 +0100

    LFU CACHE implementation

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""LRU cache module."""
+"""LFU cache module."""
 
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class LFUCache(BaseCaching):
     """Last Recently Used cache class to create instance."""
+
+    def __init__(self):
+        """Initialize lfu cache instance."""
+        super().__init__()
+        self.key_freq = {}
 
     def put(self, key, item):
         """Put method for cache insertion.
@@ -16,15 +21,24 @@ class LRUCache(BaseCaching):
         """
         if not key or not item:
             return
+
+        if not self.cache_data.get(key) \
+                and len(self.cache_data) + 1 > self.MAX_ITEMS:
+            di = self.cache_data
+            k = min(di, key=lambda x: self.key_freq.get(x))
+            print(self.key_freq)
+
+            di.pop(k)
+            self.key_freq.pop(k)
+            print(f"DISCARD: {k}")
+
         if self.cache_data.get(key):
             self.cache_data.pop(key)
         self.cache_data[key] = item
-
-        if len(self.cache_data) > self.MAX_ITEMS:
-            di = self.cache_data
-            k = [*di.keys()][-2]
-            di.pop(k)
-            print(f"DISCARD: {k}")
+        if self.key_freq.get(key) is None:
+            self.key_freq[key] = 0
+        else:
+            self.key_freq[key] = self.key_freq.get(key) + 1
 
     def get(self, key):
         """Get data with key.
@@ -33,7 +47,6 @@ class LRUCache(BaseCaching):
         key: identifier for item to get from cache
         Return: item or None
         """
-
         item = self.cache_data.get(key)
         if item:
             self.put(key, item)
