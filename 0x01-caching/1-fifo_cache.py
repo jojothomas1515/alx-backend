e8d2de4a10d5fbdebbcacbcc3e33bd400b476b49
Jojo <jojothomas1515@gmail.com>
2023-08-23 07:59:42 +0100

    lifo and fifo implemented

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""FIFO."""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Last In First Out."""
+
+    def put(self, key, item):
+        """."""
+        if not key and not item:
+            return
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            cd = self.cache_data
+            f_key = [*cd.keys()][0]
+            cd.pop(f_key)
+            print(f"DISCARD: {f_key}")
+
+    def get(self, key):
+        """Get data with key."""
+        return self.cache_data.get(key)
