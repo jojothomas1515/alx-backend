2c524bb549f16ca73b30070a9f2c3890a451b426
Jojo Thomas <jojothomas1515@gmail.com>
2023-09-26 22:43:54 +0100

    initial setup, dump.rdb added and task one done

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,7 @@
+import { createClient } from "redis";
+
+const client = createClient()
+  .on("error", (error) => {
+    console.log("Redis client not connected to the server: ", error.message);
+  })
+  .on("connect", () => console.log("Redis client connected to the server"));
